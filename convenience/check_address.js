c9c1481ae8015c93e30e2b5b42ea8fbd568fcc7e
fhildeb <felix@lukso.io>
2022-06-21 22:07:19 +0200

    add address check up

---
+++ convenience/check_address.js
@@ -0,0 +1,20 @@
+// Import and Network Setup
+const Web3 = require("web3");
+const web3 = new Web3("https://rpc.l14.lukso.network");
+
+// Static address
+const SAMPLE_PROFILE_ADDRESS = "0x0C03fBa782b07bCf810DEb3b7f0595024A444F4e";
+
+/*
+ * Check if input is a valid blockchain address
+ *
+ * @param address input
+ * @return boolean result
+ */
+function isValidAddress(address) {
+  const formattedAddress = web3.utils.toChecksumAddress(address);
+  return web3.utils.checkAddressChecksum(formattedAddress);
+}
+
+// Debug
+console.log(isValidAddress(SAMPLE_PROFILE_ADDRESS));
