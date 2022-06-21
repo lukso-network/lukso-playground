a50228b715f5376a4cd52e35d9c94f8c8af8fadb
fhildeb <felix@lukso.io>
2022-06-21 12:27:47 +0200

    add standalone fetch owned assets

---
+++ asset/current/03-fetch-owned-assets.js
@@ -0,0 +1,67 @@
+// Imports
+const Web3 = require("web3");
+const { ERC725 } = require("@erc725/erc725.js");
+require("isomorphic-fetch");
+const erc725schema = require("@erc725/erc725.js/schemas/LSP3UniversalProfileMetadata.json");
+const LSP8 = require("@lukso/lsp-smart-contracts/artifacts/LSP8IdentifiableDigitalAsset.json");
+
+// Sample Addresses
+const SAMPLE_PROFILE_ADDRESS = "0x0Ac71c67Fa5E4c9d4af4f99d7Ad6132936C2d6A3";
+
+// Network & Storage
+const RPC_ENDPOINT = "https://rpc.l14.lukso.network";
+const IPFS_GATEWAY = "https://2eff.lukso.dev/ipfs/";
+
+// Parameters for ERC725 Instance
+const provider = new Web3.providers.HttpProvider(RPC_ENDPOINT);
+const config = { ipfsGateway: IPFS_GATEWAY };
+
+// Setup Web3
+const web3 = new Web3("https://rpc.l14.lukso.network");
+
+/*
+ * Fetch the @param's Universal Profile's
+ * LSP5 data for received assets
+ *
+ * @param address of Universal Profile
+ * @return string JSON or custom error
+ */
+async function fetchReceivedAssets(address) {
+  try {
+    const profile = new ERC725(erc725schema, address, provider, config);
+    return await (
+      await profile.fetchData("LSP5ReceivedAssets[]")
+    ).value;
+  } catch (error) {
+    return console.log("This is not an ERC725 Contract");
+  }
+}
+
+/*
+ * Return array of blockchain addresses
+ * of assets that are owned by the
+ * Univeral Profile.
+ *
+ * @param owner Universal Profile address
+ * @return address[] of owned assets
+ */
+async function fetchOwnedAssets(owner) {
+  const digitalAssets = await fetchReceivedAssets(owner);
+  const ownedAssets = [];
+
+  for (let i = 0; i < digitalAssets.length; i++) {
+    // Create instance of the asset to check owner balance
+    const LSP8Contract = new web3.eth.Contract(LSP8.abi, digitalAssets[i]);
+
+    const isCurrentOwner = await LSP8Contract.methods.balanceOf(owner).call();
+    if (isCurrentOwner > 0) {
+      ownedAssets[ownedAssets.length] = digitalAssets[i];
+    }
+  }
+  return ownedAssets;
+}
+
+// Debug
+fetchOwnedAssets(SAMPLE_PROFILE_ADDRESS).then((ownedAssets) =>
+  console.log(ownedAssets)
+);
