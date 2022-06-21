a078a27eb17a8792b9f72a2320a7e6ad63b035f6
fhildeb <felix@lukso.io>
2022-06-21 12:45:28 +0200

    add standalone fetch asset data

---
+++ asset/current/08-fetch-asset-data.js
@@ -0,0 +1,110 @@
+// Imports
+const Web3 = require("web3");
+const { ERC725 } = require("@erc725/erc725.js");
+require("isomorphic-fetch");
+const LSP4schema = require("@erc725/erc725.js/schemas/LSP4DigitalAsset.json");
+const LSP4 = require("@lukso/lsp-smart-contracts/artifacts/LSP4DigitalAssetMetadata.json");
+
+// Sample Addresses
+const SAMPLE_ASSET_ADDRESS = "0xfE85568Fea15A7ED3c56F7ca6544F2b96Aeb1774";
+
+// Network & Storage
+const RPC_ENDPOINT = "https://rpc.l14.lukso.network";
+const IPFS_GATEWAY = "https://2eff.lukso.dev/ipfs/";
+
+// Parameters for ERC725 Instance
+const provider = new Web3.providers.HttpProvider(RPC_ENDPOINT);
+const config = { ipfsGateway: IPFS_GATEWAY };
+
+// Setup Web3
+const web3 = new Web3("https://rpc.l14.lukso.network");
+
+// Keys for asset properties
+const TokenNameKey = LSP4schema[1].key;
+const TokenSymbolKey = LSP4schema[2].key;
+const MetaDataKey = LSP4schema[3].key;
+const CreatorsKey = LSP4schema[4].key;
+
+/*
+ * Fetch the dataset of an asset
+ *
+ * @param key of asset property
+ * @return string of encoded data
+ */
+async function getAssetData(key, address) {
+  try {
+    // Instantiate Digital Asset smart contract
+    const digitalAsset = new web3.eth.Contract(LSP4.abi, address);
+
+    // Fetch the encoded contract data
+    return await digitalAsset.methods["getData(bytes32)"](key).call();
+  } catch (error) {
+    return console.error("Data of assets address could not be loaded");
+  }
+}
+
+/*
+ * Decode value from ERC725Y storage
+ * based on it's key and phrase
+ *
+ * @param key of asset property
+ * @param decodePhrase string of fetchable content
+ * @return string of decoded data
+ */
+async function decodeAssetData(keyName, encodedData) {
+  try {
+    // instance for the digital asset with erc725.js
+    const digitalAsset = new ERC725(
+      LSP4schema,
+      SAMPLE_ASSET_ADDRESS,
+      provider,
+      config
+    );
+
+    // Decode the assets data
+    return digitalAsset.decodeData({
+      keyName: keyName,
+      value: encodedData,
+    });
+  } catch (error) {
+    console.log("Data of an asset could not be decoded");
+  }
+}
+
+/*
+ * Create a fetchable link for the asset data
+ * that was embeded into the JSON metadata
+ *
+ * @return string of asset data URL
+ */
+async function getMetaDataLink(decodedAssetMetadata) {
+  try {
+    // Generate IPFS link from decoded metadata
+    return IPFS_GATEWAY + decodedAssetMetadata.value.url.substring(7);
+  } catch (error) {
+    console.log("URL could not be fetched");
+  }
+}
+
+/*
+ * Fetch the asset data from storage
+ *
+ * @return string with asset data as JSON
+ */
+async function fetchAssetData(dataURL) {
+  try {
+    const response = await fetch(dataURL);
+    return await response.json();
+  } catch (error) {
+    console.log("JSON data of IPFS link could not be fetched");
+  }
+}
+
+// Debug
+getAssetData(MetaDataKey, SAMPLE_ASSET_ADDRESS).then((encodedData) => {
+  decodeAssetData(MetaDataKey, encodedData).then((decodedData) => {
+    getMetaDataLink(decodedData).then((dataURL) => {
+      fetchAssetData(dataURL).then((assetJSON) => console.log(assetJSON));
+    });
+  });
+});
