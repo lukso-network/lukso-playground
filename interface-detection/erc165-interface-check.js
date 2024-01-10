1a7dd2a6b8ef160bd05c0d02f0b17ed2abd4d9c1
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2024-01-10 12:45:51 +0100

    update interface detection guide

--- interface-detection/erc165-interface-check.js
+++ interface-detection/erc165-interface-check.js
@@ -1,20 +1,44 @@
 // Imports
 import { ethers } from 'ethers';
+import { ERC725 } from '@erc725/erc725.js';
+import lsp4Schema from '@erc725/erc725.js/schemas/LSP4DigitalAsset.json' assert { type: 'json' };
 import UniversalProfile from '@lukso/lsp-smart-contracts/artifacts/UniversalProfile.json' assert { type: 'json' };
 import { INTERFACE_IDS } from '@lukso/lsp-smart-contracts/dist/constants.cjs.js';
 
-// Connect to the LUKSO Testnet
+// Asset Example
+const myAsset = new ERC725(
+  lsp4Schema,
+  '0x6395b330F063F96579aA8F7b59f2584fb9b6c3a5',
+  'https://rpc.testnet.lukso.gateway.fm',
+  {
+    ipfsGateway: 'https://api.universalprofile.cloud/ipfs',
+  },
+);
+
+const isLSP7 = await myAsset.supportsInterface(INTERFACE_IDS.LSP7DigitalAsset);
+
+const isLSP8 = await myAsset.supportsInterface(
+  INTERFACE_IDS.LSP8IdentifiableDigitalAsset,
+);
+
+console.log(isLSP7, isLSP8); // true or false
+
+// Profile Example
 const provider = new ethers.providers.JsonRpcProvider(
   'https://rpc.testnet.lukso.network',
 );
 
 // Create an instance of the Universal Profile
-const contractAddress = '0x9139def55c73c12bcda9c44f12326686e3948634';
-const myLSPContract = new ethers.Contract(
-  contractAddress,
+const myProfile = new ethers.Contract(
+  '0x9139def55c73c12bcda9c44f12326686e3948634',
   UniversalProfile.abi,
   provider,
 );
+
+const isLSP0 = await myProfile.supportsInterface(
+  INTERFACE_IDS.LSP0ERC725Account,
+);
+console.log(isLSP0); // true or false
 
 /*
 Supported interfaces from lsp-smart-contracts library:
@@ -33,8 +57,3 @@ INTERFACE_IDS.LSP17Extendable               INTERFACE_IDS.LSP17Extension
 INTERFACE_IDS.LSP20CallVerification         INTERFACE_IDS.LSP20CallVerifier
 INTERFACE_IDS.LSP25ExecuteRelayCall 
 */
-
-const supportsInterface = await myLSPContract.supportsInterface(
-  INTERFACE_IDS.LSP0ERC725Account,
-);
-console.log(supportsInterface); // true or false
