e70e566a625c938687d7269ecc315fe940267082
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2024-01-12 18:00:28 +0100

    update fetch-json-data.js

--- fetch-profile/fetch-json-data.js
+++ fetch-profile/fetch-json-data.js
@@ -4,7 +4,7 @@ import lsp3ProfileSchema from '@erc725/erc725.js/schemas/LSP3ProfileMetadata.jso
 // Initatiate erc725.js
 const erc725js = new ERC725(
   lsp3ProfileSchema,
-  '0x9139def55c73c12bcda9c44f12326686e3948634', // UP Contract Address
+  '0xEda145b45f76EDB44F112B0d46654044E7B8F319', // UP Contract Address
   'https://rpc.testnet.lukso.gateway.fm',
   {
     ipfsGateway: 'https://api.universalprofile.cloud/ipfs',
@@ -13,7 +13,7 @@ const erc725js = new ERC725(
 
 // Download and verify the profile metadata JSON file
 let profileMetaData = await erc725js.fetchData('LSP3Profile');
-console.log(profileMetaData);
+console.log(JSON.stringify(profileMetaData, undefined, 2));
 
 // Fetch all of the profile's issued assets
 let issuedAssetsDataKey = await erc725js.fetchData('LSP12IssuedAssets[]');
