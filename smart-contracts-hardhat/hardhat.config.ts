379429e939a835544d955233358260cdb5041b96
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2024-02-06 18:42:16 +0100

    add mainnet and etherscan to config

--- smart-contracts-hardhat/hardhat.config.ts
+++ smart-contracts-hardhat/hardhat.config.ts
@@ -5,6 +5,7 @@ import '@nomicfoundation/hardhat-toolbox';
 LoadEnv();
 
 const config: HardhatUserConfig = {
+  // Default compiler version for all contracts
   solidity: '0.8.19',
   networks: {
     luksoTestnet: {
@@ -12,6 +13,32 @@ const config: HardhatUserConfig = {
       chainId: 4201,
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
+    luksoMainnet: {
+      url: 'https://rpc.lukso.gateway.fm',
+      chainId: 42,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: 'no-api-key-needed',
+    customChains: [
+      {
+        network: 'luksoTestnet',
+        chainId: 4201,
+        urls: {
+          apiURL: 'https://api.explorer.execution.testnet.lukso.network/api',
+          browserURL: 'https://explorer.execution.testnet.lukso.network/',
+        },
+      },
+      {
+        network: 'luksoMainnet',
+        chainId: 42,
+        urls: {
+          apiURL: 'https://api.explorer.execution.mainnet.lukso.network/api',
+          browserURL: 'https://explorer.execution.mainnet.lukso.network/',
+        },
+      },
+    ],
   },
 };
 
