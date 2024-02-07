b62d6a52dd796e0a22cc5a053cb480f5ad6bca5b
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2024-02-07 11:04:24 +0100

    update to ethers v6

--- smart-contracts-hardhat/scripts/deployUP.ts
+++ smart-contracts-hardhat/scripts/deployUP.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 async function main() {
   // setup provider
-  const provider = new ethers.providers.JsonRpcProvider('https://rpc.testnet.lukso.gateway.fm');
+  const provider = new ethers.JsonRpcProvider('https://rpc.testnet.lukso.gateway.fm');
 
   // setup signer (the browser extension controller)
   const signer = new ethers.Wallet(process.env.PRIVATE_KEY as string, provider);
@@ -19,15 +19,15 @@ async function main() {
 
   const tokenFactory = await ethers.getContractFactory('MyCustomToken');
 
-  const deplyedTransaction = await tokenFactory.getDeployTransaction([
-    'My Custom Token', // token name
-    'MCT', // token symbol
-    signer.address, // token owner
-    0, // token type = TOKEN
-    false, // isNonDivisible?
-  ]);
+  const deployTx = tokenFactory.getDeployTransaction(
+    "My Custom Token", // token name
+    "MCT",             // token symbol
+    signer.address,    // token owner
+    0,                 // token type = TOKEN
+    false              // isNonDivisible?
+  );
 
-  console.log('test: ', deplyedTransaction);
+  console.log('test: ', deployTx);
 
   /**
    * Custom LSP7 Token
@@ -38,12 +38,12 @@ async function main() {
   // get the address of the contract that will be created
   const CustomTokenAddress = await UP.connect(signer)
     .getFunction('execute')
-    .staticCall(1, ethers.constants.AddressZero, 0, CustomTokenBytecode);
+    .staticCall(1, ethers.ZeroAddress, 0, CustomTokenBytecode);
 
   // deploy CustomLSP7 as the UP (signed by the browser extension controller)
   const tx1 = await UP.connect(signer).getFunction('execute')(
     1,
-    ethers.constants.AddressZero,
+    ethers.ZeroAddress,
     0,
     CustomTokenBytecode,
   );
