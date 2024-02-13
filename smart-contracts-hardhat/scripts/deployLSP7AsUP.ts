758d1a91e8722d5143bf989f2427080302104e87
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2024-02-13 21:41:11 +0100

    Update deployment docs guides

--- smart-contracts-hardhat/scripts/deployLSP7AsUP.ts
+++ smart-contracts-hardhat/scripts/deployLSP7AsUP.ts
@@ -1,15 +1,15 @@
-import hre from 'hardhat';
 import { ethers } from 'hardhat';
 import * as dotenv from 'dotenv';
+
 import LSP0Artifact from '@lukso/lsp-smart-contracts/artifacts/LSP0ERC725Account.json';
 
-// load env vars
+// Load environment variables
 dotenv.config();
 
 async function deployToken() {
-  // Setup the controller used to sign the deployment
-  const [signer] = await ethers.getSigners();
-  console.log('Deploying contracts with Universal Profile Controller: ', signer.address);
+  // UP controller used for deployment
+  const [deployer] = await ethers.getSigners();
+  console.log('Deploying contract with Universal Profile controller: ', deployer.address);
 
   // Load the Universal Profile
   const universalProfile = await ethers.getContractAtFromArtifact(
@@ -17,16 +17,11 @@ async function deployToken() {
     process.env.UP_ADDR as string,
   );
 
-  /**
-   * Custom LSP7 Token
-   */
-
   // Create custom bytecode for the token deployment
-  const CustomTokenBytecode = hre.artifacts.readArtifactSync('MyCustomToken').bytecode;
-
+  const tokenBytecode = (await ethers.getContractFactory('MyCustomToken')).bytecode;
   const abiEncoder = new ethers.AbiCoder();
 
-  // Encode constructor params
+  // Encode constructor parameters
   const encodedConstructorParams = abiEncoder.encode(
     ['string', 'string', 'address', 'uint256', 'bool'],
     [
@@ -38,34 +33,28 @@ async function deployToken() {
     ],
   );
 
-  // Add the constructor params to the Custom Token bytecode
-  const CustomTokenBytecodeWithConstructor =
-    CustomTokenBytecode + encodedConstructorParams.slice(2);
+  // Add the constructor parameters to the token bytecode
+  const tokenBytecodeWithConstructor = tokenBytecode + encodedConstructorParams.slice(2);
 
   // Get the address of the custom token contract that will be created
-  const CustomTokenAddress = await universalProfile
-    .connect(signer)
-    .getFunction('execute')
-    .staticCall(
-      1, // Operation type: CREATE
-      ethers.ZeroAddress,
-      0, // Value is empty
-      CustomTokenBytecodeWithConstructor,
-      { gasLimit: 10000000 },
-    );
-
-  // Deploy the contract by the Universal Profile
-  const tx = await universalProfile.connect(signer).getFunction('execute')(
+  const customTokenAddress = await universalProfile.execute.staticCall(
     1, // Operation type: CREATE
-    ethers.ZeroAddress,
+    ethers.ZeroAddress, // Target: 0x0 as contract will be initialized
     0, // Value is empty
-    CustomTokenBytecodeWithConstructor,
-    { gasLimit: 10000000 },
+    tokenBytecodeWithConstructor, // Payload of the contract
   );
 
-  await tx.wait();
+  // Deploy the contract by the Universal Profile
+  const tx = await universalProfile.execute(
+    1, // Operation type: CREATE
+    ethers.ZeroAddress, // Target: 0x0 as contract will be initialized
+    0, // Value is empty
+    tokenBytecodeWithConstructor, // Payload of the contract
+  );
 
-  console.log('Custom token address: ', CustomTokenAddress);
+  // Wait for the transaction to be included in a block
+  await tx.wait();
+  console.log('Token deployed at: ', customTokenAddress);
 }
 
 deployToken()
