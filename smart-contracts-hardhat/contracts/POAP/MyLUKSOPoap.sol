bc2d18820552fa472c074cb2f0f72df98ce568ca
CJ42 <cavallerajean@gmail.com>
2024-02-23 09:56:57 +0000

    refactor: use sample verifiable URI as constant

--- smart-contracts-hardhat/contracts/POAP/MyLUKSOPoap.sol
+++ smart-contracts-hardhat/contracts/POAP/MyLUKSOPoap.sol
@@ -1,20 +1,25 @@
 // SPDX-License-Identifier: Apache-2.0
+
 pragma solidity ^0.8.20;
 
 // modules
+
 import {
     LSP8IdentifiableDigitalAsset as LSP8
 } from "@lukso/lsp-smart-contracts/contracts/LSP8IdentifiableDigitalAsset/LSP8IdentifiableDigitalAsset.sol";
 
 // libraries
+
 import {
     LSP2Utils
 } from "@lukso/lsp-smart-contracts/contracts/LSP2ERC725YJSONSchema/LSP2Utils.sol";
 
 // constants
+
 import {
     _INTERFACEID_LSP0
 } from "@lukso/lsp-smart-contracts/contracts/LSP0ERC725Account/LSP0Constants.sol";
+
 import {
     _LSP4_TOKEN_TYPE_NFT,
     _LSP4_METADATA_KEY,
@@ -22,6 +27,7 @@ import {
     _LSP4_CREATORS_ARRAY_KEY,
     _LSP4_CREATORS_MAP_KEY_PREFIX
 } from "@lukso/lsp-smart-contracts/contracts/LSP4DigitalAssetMetadata/LSP4Constants.sol";
+
 import {
     _LSP8_TOKENID_FORMAT_UNIQUE_ID
 } from "@lukso/lsp-smart-contracts/contracts/LSP8IdentifiableDigitalAsset/LSP8Constants.sol";
@@ -33,15 +39,21 @@ function generateKeccak256Utf8VerifiableURI(
     return
         abi.encodePacked(
             // verifiableURI identifier
+
             bytes2(0x0000),
             // verification method ID.
+
             // first 4 bytes of the keccak256 hash of the string "keccak256(utf8)"
+
             bytes4(0x6f357c6a),
             // verification data length
+
             bytes2(0x0020),
             // verification data
+
             verificationDataHash,
             // encoded URI
+
             uri
         );
 }
@@ -56,33 +68,33 @@ contract MyLUKSOPoap is
     )
 {
     // using {generateArrayElementKeyAtIndex} for bytes32;
+
     using LSP2Utils for *;
 
     /// @notice Replace the address below with your UP address here to define yourself as the creator.
+
     /// @dev For people who do not have a UP, simply leave this address for demo purpose (it is my UP address).
+
     address constant WORKSHOP_CREATOR =
-        0x38382B0437453004AC1af86839249037b130417B;
+        0x87cC4Edf3E4EfF2eD959Db21e2C1295D401B7983;
 
-    constructor(
-        bytes32 verificationDataHash,
-        string memory poapMetadataIPFSURI
-    ) {
-        bytes memory poapVerifiableURI = generateKeccak256Utf8VerifiableURI(
-            verificationDataHash,
-            poapMetadataIPFSURI
-        );
+    bytes constant VERIFIABLE_URI =
+        hex"00006f357c6a0020f54eb9ca225c71ad8631576512a36c72bc9a367164545e7b9e4c6f9e770dabc2697066733a2f2f516d556d747a67394361416872786170474a774a7969756f724d625a7279616a6e326970756d6933325663593748";
 
-        _setData(_LSP4_METADATA_KEY, poapVerifiableURI);
+    constructor() {
+        _setData(_LSP4_METADATA_KEY, VERIFIABLE_URI);
 
         // There is only one creator: yourself!
 
         // Set the LSP4 Creator Array length to 1
+
         _setData(
             _LSP4_CREATORS_ARRAY_KEY,
             abi.encodePacked(bytes16(uint128(1)))
         );
 
         // Set your UP address under the LSP4_CREATORS[] at index 0
+
         bytes32 lsp4CreatorArrayAtIndex = _LSP4_CREATORS_ARRAY_KEY
             .generateArrayElementKeyAtIndex(0);
 
@@ -99,10 +111,13 @@ contract MyLUKSOPoap is
 
     function claim() external {
         bytes32 tokenId = keccak256(abi.encode(msg.sender));
+
         _mint(msg.sender, tokenId, true, "");
 
         // set the same metadata for the tokenId than the LSP4Metadata
+
         bytes memory lsp4MetadataValue = _getData(_LSP4_METADATA_KEY);
+
         _setDataForTokenId(tokenId, _LSP4_METADATA_KEY, lsp4MetadataValue);
     }
 
--- smart-contracts-hardhat/scripts/POAP/upload-poap-image.ts
+++ smart-contracts-hardhat/scripts/POAP/upload-poap-image.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import FormData from 'form-data';
 import fs from 'fs';
-const JWT =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIyYTM5NzMzNS1kNDYyLTRlODQtYWFjZC00NmZkN2M4ZTY3NmIiLCJlbWFpbCI6ImplYW5AbHVrc28uaW8iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJpZCI6IkZSQTEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX0seyJpZCI6Ik5ZQzEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiYjUzY2JiZTZkNjM4ODQ0ODU2MTkiLCJzY29wZWRLZXlTZWNyZXQiOiJmMmQwM2NkOGRhMTI1MGM5MjJjOTE5NGE0Y2Q4MGRjYTE3MWIwNGVhM2UwYTZkNzNkMGQ3N2JkMjJiZGEzN2RlIiwiaWF0IjoxNzA4NTExODc1fQ.j87B0bCNa0jkJ3ID5GZjn6rWZLR8mI0bcHJQSHu5YVI';
+const JWT = 'your-piñata-jwt-token';
 
 const pinFileToIPFS = async () => {
   const formData = new FormData();
@@ -22,13 +21,17 @@ const pinFileToIPFS = async () => {
   formData.append('pinataOptions', pinataOptions);
 
   try {
-    const image = await axios.post('https://api.pinata.cloud/pinning/pinFileToIPFS', formData, {
-      //   maxBodyLength: 'Infinity',
-      headers: {
-        'Content-Type': `multipart/form-data; boundary=${formData.getBoundary()}`,
-        Authorization: `Bearer ${JWT}`,
+    const image = await axios.post(
+      'https://api.pinata.cloud/pinning/pinFileToIPFS',
+      formData,
+      {
+        //   maxBodyLength: 'Infinity',
+        headers: {
+          'Content-Type': `multipart/form-data; boundary=${formData.getBoundary()}`,
+          Authorization: `Bearer ${JWT}`,
+        },
       },
-    });
+    );
     console.log(image.data);
   } catch (error) {
     console.log(error);
