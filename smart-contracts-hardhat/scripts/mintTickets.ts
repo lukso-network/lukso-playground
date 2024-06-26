ef5697379042ebf85da95cea9da286de066a7775
CJ42 <cavallerajean@gmail.com>
2024-06-26 11:24:36 +0100

    chore: fix linting errors in smart contracts

--- smart-contracts-hardhat/scripts/mintTickets.ts
+++ smart-contracts-hardhat/scripts/mintTickets.ts
@@ -8,12 +8,11 @@ async function deployAndCreateTickets() {
   const accounts = await ethers.getSigners();
   const deployer = accounts[0];
 
-  const luksoMeetupTickets: EventTicketsNFT =
-    await new EventTicketsNFT__factory(deployer).deploy(
-      'LUKSO Meetup #2',
-      'MUP2',
-      deployer.address,
-    );
+  const luksoMeetupTickets: EventTicketsNFT = await new EventTicketsNFT__factory(deployer).deploy(
+    'LUKSO Meetup #2',
+    'MUP2',
+    deployer.address,
+  );
 
   // create 100 entry tickets.
   // Give them to the deployer initially, who will distribute them afterwards.
@@ -26,10 +25,7 @@ async function deployAndCreateTickets() {
 
   const receipt = await tx.wait();
 
-  console.log(
-    'Succesfully deployed and minted 100 NFT tickets! Receipt: ',
-    receipt,
-  );
+  console.log('Succesfully deployed and minted 100 NFT tickets! Receipt: ', receipt);
 }
 
 deployAndCreateTickets().catch((error) => {
